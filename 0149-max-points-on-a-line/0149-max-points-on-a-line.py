1a8999d54a6bc618c4b780eb0083a056bcd89859
Shiv Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-10-05 14:53:14 +0530

    Time: 67 ms (87.86%), Space: 16.2 MB (90.50%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) <= 2:
+            return len(points)
+
+        max_points = 2  # At least two points will always be on the same line
+
+        for i in range(len(points)):
+            slope_count = defaultdict(int)
+            duplicate = 0
+            local_max = 0
+
+            for j in range(i + 1, len(points)):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+
+                if x1 == x2 and y1 == y2:
+                    duplicate += 1
+                elif x1 == x2:
+                    slope = 'inf'
+                    slope_count[slope] += 1
+                    local_max = max(local_max, slope_count[slope])
+                else:
+                    slope = (y2 - y1) / (x2 - x1)
+                    slope_count[slope] += 1
+                    local_max = max(local_max, slope_count[slope])
+
+            max_points = max(max_points, local_max + duplicate + 1)
+
+        return max_points
