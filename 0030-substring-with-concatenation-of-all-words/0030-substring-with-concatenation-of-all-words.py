3aa5c4b0e9855038f0885d07f543d09c5df6094a
Shiv Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-10-13 23:21:06 +0530

    Time: 116 ms (68.88%), Space: 17 MB (45.34%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not s or not words:
+            return []
+
+        word_len = len(words[0])
+        total_len = word_len * len(words)
+        word_count = Counter(words)
+        result = []
+
+        for i in range(word_len):
+            left = i
+            right = i
+            current_count = Counter()
+
+            while right + word_len <= len(s):
+                word = s[right:right + word_len]
+                right += word_len
+                current_count[word] += 1
+
+                while current_count[word] > word_count[word]:
+                    left_word = s[left:left + word_len]
+                    current_count[left_word] -= 1
+                    left += word_len
+
+                if right - left == total_len:
+                    result.append(left)
+
+        return result
