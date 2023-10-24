e0921b8d548e544a8ac5cce6d8d923bd099730d3
Shivnath Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-10-24 08:19:47 +0530

    Time: 66 ms (14.71%), Space: 16.4 MB (16.71%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,21 +1,21 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        # x = 0 #as Initially, the value of X is 0.
-        # hash_map = {
-        #     '++X':'+1',
-        #     'X++':'+1',
-        #     'X--':'-1',
-        #     '--X':'-1'
-        # }
-        # y = ''
-        # for i in operations:
-        #     y +=hash_map[i]
-        # return eval(y)
-        X = 0
-        for op in operations:
-            if "++" in op:
-                X += 1
-            else:
-                X -= 1
-        return X
+        x = 0 #as Initially, the value of X is 0.
+        hash_map = {
+            '++X':'+1',
+            'X++':'+1',
+            'X--':'-1',
+            '--X':'-1'
+        }
+        y = ''
+        for i in operations:
+            y +=hash_map[i]
+        return eval(y)
+        # X = 0
+        # for op in operations:
+        #     if "++" in op:
+        #         X += 1
+        #     else:
+        #         X -= 1
+        # return X
             
