f790d3c7824f8289c79af47673cc9e2a492d2a6d
Shivnath Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-10-30 17:05:17 +0530

    Time: 40 ms (41.73%), Space: 16.4 MB (11.35%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,12 +1,6 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-   
         words = s.split()
-
-
         words.sort(key=lambda word: int(word[-1]))
-
-
         original_sentence = ' '.join([word[:-1] for word in words])
-
         return original_sentence
